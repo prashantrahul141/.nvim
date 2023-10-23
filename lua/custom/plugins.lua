bffc60215b4c3f5a27bdccde58f63447a955a918
Prashant <prashantrahul141@protonmail.com>
2023-10-23 12:55:40 +0530

    add

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,82 @@
+local plugins = {
+  -- transparent background
+{
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = function()
+      require "transparent"
+    end,
+  },
+
+  -- syntax highlighting using treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- common
+        "bash",
+
+        -- web dev
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+
+        -- c/cpp
+        "cpp",
+        "c",
+        "cmake",
+
+        -- rust
+        "rust",
+        "toml",
+
+        -- py
+        "python",
+      },
+    },
+  },
+
+  -- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  -- lsp
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "marksman",
+
+        --web
+        "html",
+        "cssls",
+        "tsserver",
+        "eslint-lsp",
+
+        "prettier",
+        "stylua",
+      },
+    },
+  },
+
+  
+  
+}
+
+return plugins
