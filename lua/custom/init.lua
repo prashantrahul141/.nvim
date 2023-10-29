16578cd8bda54f1689a069b52ef8f0da23ee041a
Prashant <prashantrahul141@protonmail.com>
2023-10-29 23:44:35 +0530

    use nushell instead

---
+++ lua/custom/init.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+-- remember to install xclip
+-- sudo apt install xclip
+-- to enable system clipboard.
+opt.clipboard = "unnamedplus"
+
+-- use nushell instead
+opt.shell = "/home/linuxbrew/.linuxbrew/bin/nu"
